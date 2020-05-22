9c105aa7cd84042e487f08a9dc6747e110296c09
Dani Santos <santos.adaniele@gmail.com>
2020-05-22 12:01:35 -0400

    refactor test to user helper function

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,20 +1,38 @@
 package wallet
 
+import "fmt"
+
+// Bitcoin type
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+// Stringer interface
+type Stringer interface {
+	String() string
+}
+
 // Wallet struct
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // Deposit allows to deposit amount to wallet
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	// fmt.Printf("address of balance in Deposit is %v\n", &w.balance)
 	(*w).balance += amount
 	// we can just write w.balance
 	//struct pointers are automatically dereferenced
+}
 
+// Withdraw allows to withdraw amount from wallet
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	(*w).balance -= amount
 }
 
 // Balance allows to get value from wallet
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return (*w).balance
 }
--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -5,16 +5,27 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := &Wallet{}
 
-	wallet.Deposit(10)
+	assertBalance := func(t *testing.T, wallet *Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
 
-	got := wallet.Balance()
-	want := 10
-
-	// fmt.Printf("address of balance in test is %v\n", &wallet.balance)
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := &Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+		// fmt.Printf("address of balance in test is %v\n", &wallet.balance)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := &Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 
 }
