735cfbbbce58a878931ba52babc0979e85cc0d0c
Dani Santos <santos.adaniele@gmail.com>
2020-05-23 19:48:35 -0400

    adds more spies

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -42,3 +42,19 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
+
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
