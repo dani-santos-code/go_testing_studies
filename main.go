17a3cf9dea8f8c4578d6b4aa8cb010f611882724
Dani Santos <santos.adaniele@gmail.com>
2020-06-07 10:18:43 -0400

    concurrency

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
-import "github.com/dani-santos-code/go_testing_studies/countdown"
-import "os"
+import (
+	"os"
 
+	"github.com/dani-santos-code/go_testing_studies/countdown"
+)
 
 func main() {
 	countdown.Countdown(os.Stdout, &countdown.DefaultSleeper{})
