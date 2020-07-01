59fc3d70ad260493281bf73ac394d07c28d8b4b6
Dani Santos <santos.adaniele@gmail.com>
2020-07-01 11:26:46 -0400

    reflection

---
+++ reflection/reflect_test.go
@@ -0,0 +1,52 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				Job  string
+			}{"Joshua", "programmer"},
+			[]string{"Joshua", "programmer"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				Age  int
+			}{"Joshua", 32},
+			[]string{"Joshua"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
